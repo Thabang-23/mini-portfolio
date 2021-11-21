00cfc2fb46dd14c1a5ca49647dad0fee440a3a62
Thabang khoza <tkhoza077@gmail.com>
2021-11-21 20:11:39 +0200

    Fixed Errors

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ export default function About() {
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
               <img
                 className="object-cover object-center rounded"
-                alt="my photo"
+                alt="mine"
                 src="./photo.jpg"
               />
             </div>
--- src/components/Sovtech.js
+++ src/components/Sovtech.js
@@ -1,4 +1,4 @@
-import { OfficeBuildingIcon } from "@heroicons/react/solid";
+
 import React from "react";
 
 export default function Sovtech() {
